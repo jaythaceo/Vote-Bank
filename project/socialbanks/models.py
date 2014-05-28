a78506433fed5d6f3fa757979a8b6328c22fa5c3
jaythaceo <jaythaceo@gmail.com>
2014-05-27 22:48:28 -0400

    db.setup

--- project/socialbanks/models.py
+++ project/socialbanks/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Profiles(models.Model):
+
+    user_name = models.Charfield(
+            max_length=255,
+            )
+    user_city = models.Charfield(
+            max_length=255,
+            )
+    user_state = models.Charfield(
+            max_length=255,
+            )
+    email = models.EmailField()
+
+    def __str__(self):
+        return ' '.join([self.user_name, self.user_city, self.user_state,])
+
+
