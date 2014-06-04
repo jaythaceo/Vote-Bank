619141a5034d270f38274b996efb597ff64faf11
jaythaceo <jaythaceo@gmail.com>
2014-06-03 21:37:29 -0400

    db setup2

--- project/votebank/settings.py
+++ project/votebank/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'NAME': 'votebank.db',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'voter.db',
         'PASSWORD': '',
@@ -124,6 +124,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'votebank',
 )
 
 # A sample logging configuration. The only tangible logging
